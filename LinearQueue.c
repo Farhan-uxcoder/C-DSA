80be8494d56fb91509c31fc780978430ff811ec0
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2025-11-05 09:45:21 +0530

    updated the Queue with adding the isfull and isempty

--- LinearQueue.c
+++ LinearQueue.c
@@ -4,7 +4,31 @@
 int queue[MAX],front=-1,rear=-1;
 int main(){
     int ele,ch;
-    // while(1){
-    //     printf("1.Emqueue\n2.Dequeue\n3.Display\n4.Exit\n");
-    // }
+    while(1){
+        printf("1.Emqueue\n2.Dequeue\n3.Display\n4.Exit\n");
+    printf("What is your choice?\n");
+    scanf("%d",&ch);
+    switch (ch)
+    {
+        case 1:
+        printf("Enter the element to be enqueued:\n");
+        scanf("%d",&ele);
+        enqueue(ele);
+        break;
+        case 2:
+        ele=dequeue();
+    if(ele!=-1)
+    printf("The dequeued element is %d\n",ele);
+    break;
+    case 3:
+    display();
+    break;
+    case 4:
+    exit(0);
+    default:
+    printf("Invalid choice\n");
+        break;
+    }
+    }
+    return 0;
 }
