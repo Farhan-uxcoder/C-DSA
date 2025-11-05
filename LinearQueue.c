4ccd0cf7531af71ab8303091be6a56b8678d582b
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2025-11-05 09:32:03 +0530

    Added the 1st files

---
+++ LinearQueue.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 5
+int queue[MAX],front=-1,rear=-1;
+int main(){
+    int ele,ch;
+    // while(1){
+    //     printf("1.Emqueue\n2.Dequeue\n3.Display\n4.Exit\n");
+    // }
+}
